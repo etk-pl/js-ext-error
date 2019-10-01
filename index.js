d3502897dc5f9d234409f33b464a043324edce37
Maciej Lasoń <maciejlason3@gmail.com>
2019-10-01 10:45:14 +0200

    Added support for stack in constructor, Added static from method that can create error from error-like object

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ class ExtError extends Error {
 	 *
 	 * @param code
 	 * @param message
-     * @param stack
+	 * @param stack
 	 */
 	constructor(code, message, stack) {
 		super();
 		this.name = this.constructor.name;
 		this.code = code || "";
 		this.message = message || "";
-		if(stack) {
-            this.stack = stack;
-        }
+		if (stack) {
+			this.stack = stack;
+		}
 	}
 
-    /**
-     * Creates new error from error-like object
-     * @param {object} error Error-like object
-     * @returns {ExtError}
-     */
+	/**
+	 * Creates new error from error-like object
+	 * @param {object} error Error-like object
+	 * @returns {ExtError}
+	 */
 	static from(error) {
-	    if(error && typeof error === "object") {
-	        return new ExtError(error.code, error.message, error.stack);
-        }
-        return new ExtError("ERR_NOT_AN_ERRORLIKE", "Value is not error-like");
-    }
+		if (error && typeof error === "object") {
+			return new ExtError(error.code, error.message, error.stack);
+		}
+		return new ExtError("ERR_NOT_AN_ERRORLIKE", "Value is not error-like");
+	}
 
 	/**
 	 *
@@ -70,7 +70,7 @@ class ExtError extends Error {
 		return {
 			code: this.code,
 			message: this.message,
-            stack: this.stack
+			stack: this.stack
 		};
 	}
 }
